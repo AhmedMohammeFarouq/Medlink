88bc3ea1c069ee27c1141197f43640753d1a7d63
hamzaashraf1040-ui <hamza.ashraf1040@gmail.com>
2026-09-16 07:35:48 +0300

    refactor(medical-records): update controller and service method names for consistency

--- backend/src/modules/medical-records/medicalRecord.controller.js
+++ backend/src/modules/medical-records/medicalRecord.controller.js
@@ -2,7 +2,7 @@ import * as medicalRecordService from "./medicalRecord.service.js";
 import * as patientService from "../patients/patient.service.js";
 import { successResponse } from "../../utils/apiResponse.js";
 
-export const getOrCreateMedicalRecord = async (req, res, next) => {
+export const myMedicalRecord = async (req, res, next) => {
     try {
         const patient = await patientService.getPatientById(req.params.patientId);
         patientService.checkPatientAccess(req.user, patient);
--- backend/src/modules/medical-records/medicalRecord.routes.js
+++ backend/src/modules/medical-records/medicalRecord.routes.js
@@ -3,7 +3,7 @@ import authMiddleware from "../../middlewares/auth.middleware.js";
 import roleMiddleware from "../../middlewares/role.middleware.js";
 import validationMiddleware from "../../middlewares/validation.middleware.js";
 import { ROLES } from "../../constants/roles.js";
-import { getByPatientId, updateByPatientId, addAllergy } from "./medicalRecord.controller.js";
+import { myMedicalRecord, updateMedicalRecord, addAllergy } from "./medicalRecord.controller.js";
 import { validateMedicalRecordUpdate, validateAllergy } from "./medicalRecord.validation.js";
 
 const router = Router();
@@ -11,8 +11,8 @@ const router = Router();
 router.use(authMiddleware);
 router.use(roleMiddleware(ROLES.PATIENT, ROLES.DOCTOR, ROLES.SYSTEM_ADMIN));
 
-router.get("/:patientId", getByPatientId);
-router.patch("/:patientId", validationMiddleware(validateMedicalRecordUpdate), updateByPatientId);
+router.get("/:patientId", myMedicalRecord);
+router.patch("/:patientId", validationMiddleware(validateMedicalRecordUpdate), updateMedicalRecord);
 router.post("/:patientId/allergies", validationMiddleware(validateAllergy), addAllergy);
 
 export default router;
